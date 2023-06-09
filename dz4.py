bbb608dccf2489f09c407e17e4921d832950f9d1
PavelCRM <113143322+PavelCRM@users.noreply.github.com>
2023-06-10 00:51:55 +0300

    Update dz4.py

--- dz4.py
+++ dz4.py
@@ -3,20 +3,19 @@
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 def transpose(matrix):
-    
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        new_matrix = []
-        for j in range(cols):
-            new_row = []
+    rows = len(matrix)
+    cols = len(matrix[0])
+    new_matrix = []
+    for j in range(cols):
+        new_row = []
         for i in range(rows):
             new_row.append(matrix[i][j])
         new_matrix.append(new_row)
-        return new_matrix
+    return new_matrix
 
 print("Исходная матрица", matrix)
-print("Траспонированная матрица",transpose(matrix))
+print("Траспонированная матрица", transpose(matrix))
+
 
 
 #Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, где ключ - значение переданного аргумента,
