ef9ce2253718b84206bd208a12f415d7b4a35dcf
PavelCRM <nachtmahr666@yandex.ru>
2023-06-16 00:32:44 +0300

    1 commit

---
+++ Date valid/main.py
@@ -0,0 +1,10 @@
+from date_validator import is_valid_date
+
+date = input("Введите дату в формате DD.MM.YYYY: ")
+
+valid = is_valid_date(date)
+
+if valid:
+    print("Дата существует")
+else:
+    print("Дата невозможна")
